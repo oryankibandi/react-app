141be25c695883754e0212e059b78b488c40c9fc
Oryan Kibandi <shreckoryan@gmail.com>
2020-04-14 15:23:38 +0300

    Updates

---
+++ precache-manifest.30f0b566e430c66f49cd8cf6a81c5d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2c16b174704af3da144776471c75e0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8829bcbb35237cb53904",
+    "url": "/react-app/static/css/main.4380a15f.chunk.css"
+  },
+  {
+    "revision": "fbd81bc24a986afdb69b",
+    "url": "/react-app/static/js/2.219e708e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.219e708e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8829bcbb35237cb53904",
+    "url": "/react-app/static/js/main.5c399bfe.chunk.js"
+  },
+  {
+    "revision": "e180e5d1ae2ceffd9861",
+    "url": "/react-app/static/js/runtime-main.292972b0.js"
+  }
+]);
